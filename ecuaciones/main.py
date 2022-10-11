f74fec507a0057db63cb2d198a0c1698fd5162c6
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-11 10:21:58 +0200

    organizacion carpeta

---
+++ ecuaciones/main.py
@@ -0,0 +1,6 @@
+import ecuaciones.ecuacion1 as ecuacion1
+
+if __name__ == "__main__":
+    print("------------ECUACION 1-----------------")
+    ecuacion1.ecuacion1()
+    print("------------ECUACION 2-----------------")
