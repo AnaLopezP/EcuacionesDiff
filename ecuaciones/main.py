abd5a3e868fc63ac21862d96ff250b76ad05c6d1
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-11 10:50:44 +0200

    seguir en el main

--- ecuaciones/main.py
+++ ecuaciones/main.py
@@ -1,6 +1,8 @@
-import ecuaciones.ecuacion1 as ecuacion1
+import ecuacion1
+import ecuacion2
 
 if __name__ == "__main__":
     print("------------ECUACION 1-----------------")
     ecuacion1.ecuacion1()
     print("------------ECUACION 2-----------------")
+    ecuacion2.ecuacion2()
