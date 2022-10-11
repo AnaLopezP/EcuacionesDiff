15eb53fb8317a10761f6d8dddd9b8567fbf17f2f
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-11 10:19:24 +0200

    poner en funcion y main

---
+++ ecuacion1.py
@@ -0,0 +1,24 @@
+import matplotlib as plt
+import sympy
+
+
+'''Ecuacion 1:
+ dy/dx = (x^2y-y)/(y+1)'''
+
+def ecuacion1():
+#Defino las incógnitas:
+    x = sympy.symbols('x')
+    y = sympy.Function('y')
+
+    #Defino la función:
+    f = y(x).diff(x) - ((x**2)*y(x) - y(x))/(y(x)+1)
+    print(f)
+    sympy.Eq(y(x).diff(x), f)
+
+    #Condicion inicial:
+    ics = {y(3): -1}
+
+    #resolver la ecuacion:
+    sol = sympy.dsolve(f)
+    print("-------------------------SOLUCIÓN--------------------------")
+    print(sol)
