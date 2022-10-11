79505ee7492799684f562f254ef4b8b20414e995
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-11 10:48:38 +0200

    intento numero 3445

--- ecuaciones/ecuacion2.py
+++ ecuaciones/ecuacion2.py
@@ -10,12 +10,12 @@ def ecuacion2():
     y = sympy.Function('y')
 
     #Defino la función:
-    f = (y(x).diff(x))*sympy.sin(x) - (y(x)*sympy.log(y(x), sympy.exp))
+    f = (y(x).diff(x))*sympy.sin(x) - (y(x)*sympy.log(y(x), sympy.exp(1)))
     print(f)
     sympy.Eq(y(x).diff(x), f)
 
     #Condicion inicial:
-    ics = {y(sympy.pi()/2): sympy.exp}
+    ics = {y(sympy.pi()/2): sympy.exp(1)}
 
     #resolver la ecuacion:
     sol = sympy.dsolve(f)
