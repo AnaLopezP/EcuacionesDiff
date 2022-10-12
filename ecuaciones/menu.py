17a963309e28380e5588f451f17c748ae690ff66
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-12 21:00:55 +0200

    jmmmm

---
+++ ecuaciones/menu.py
@@ -0,0 +1,19 @@
+import ecuacion1
+import ecuacion2
+import ecuacion3
+import ecuacion4
+import helpers
+import sympy
+import numpy as pd
+
+
+
+if __name__ == "__main__":
+    print("------------ECUACION 1-----------------")
+    ecuacion1.ecuacion1()
+    print("------------ECUACION 2-----------------")
+    ecuacion2.ecuacion2()
+    print("------------ECUACION 3-----------------")     
+    ecuacion3.ecuacion3()   
+    print("------------ECUACION 4-----------------")
+    ecuacion4.ecuacion4()
