b425a1aa6813ef1dfb08a24d4e5bb77947751499
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-12 21:10:06 +0200

    menu terminado

--- ecuaciones/menu.py
+++ ecuaciones/menu.py
@@ -6,14 +6,38 @@ import helpers
 import sympy
 import numpy as pd
 
+def iniciar():
+    while True:
+        helpers.limpiar_pantalla()
 
+        print("QUE EJERCICIO QUIERES VER:")
+        print("[1] Ejercicio 1")
+        print("[2] Ejercicio 2")
+        print("[3] Ejercicio 3")
+        print("[4] Ejercicio 4")
+        print("[5] Ninguno")
 
-if __name__ == "__main__":
-    print("------------ECUACION 1-----------------")
-    ecuacion1.ecuacion1()
-    print("------------ECUACION 2-----------------")
-    ecuacion2.ecuacion2()
-    print("------------ECUACION 3-----------------")     
-    ecuacion3.ecuacion3()   
-    print("------------ECUACION 4-----------------")
-    ecuacion4.ecuacion4()
+        decision = input("> ")
+        helpers.limpiar_pantalla()
+
+        if decision == '1':
+            print("--------------------------------ECUACION 1----------------------------------")
+            ecuacion1.ecuacion1()
+
+        if decision == '2':
+            print("--------------------------------ECUACION 2----------------------------------")
+            ecuacion2.ecuacion2()
+
+        if decision == '3':
+            print("--------------------------------ECUACION 3----------------------------------")
+            ecuacion3.ecuacion3()
+
+        if decision == '4':
+            print("--------------------------------ECUACION 4----------------------------------")
+            ecuacion4.ecuacion4()
+        
+        if decision == '5':
+            print("Saliendo...\n")
+            break
+
+        input("\nPresiona ENTER  para continuar")
